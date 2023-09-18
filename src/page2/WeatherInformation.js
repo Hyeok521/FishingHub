14d4d867dec94e2afe4a0d1d0b1494d25a63dfdb
Dongjin Jung <jdj3879@naver.com>
2023-09-18 18:24:06 +0900

    0918_날씨정보수정_JDJ

--- src/page2/WeatherInformation.js
+++ src/page2/WeatherInformation.js
@@ -5,20 +5,64 @@ const WeatherInformation = () => {
   const mapRef = useRef(null);
   const placesRef = useRef(null);
   const markersRef = useRef([]);
-  const [placeInfo, setPlaceInfo] = useState(null);
+  const infowindowsRef = useRef([]);
+  const [hourlyForecast, setHourlyForecast] = useState([]);
+  const [forecastStart, setForecastStart] = useState(0);
 
-  // OpenWeatherMap API를 사용하여 날씨 정보를 가져오는 함수
+  // 슬라이드 방향에 따라 예보 시작 지점을 조정합니다.
+  const slideForecast = (direction) => {
+    if (direction === "left" && forecastStart > 0) {
+      setForecastStart(forecastStart - 1);
+    } else if (direction === "right" && forecastStart < 43) {
+      setForecastStart(forecastStart + 1);
+    }
+  };
+
+  // 현재 날씨 정보를 가져오는 함수입니다.
   const fetchWeatherInfo = async (lat, lng) => {
     const API_KEY = "e7f59aaca8543637eab9ad2b801f9249";
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}&lang=kr`
     );
     const data = await response.json();
     return data;
   };
 
+  // 시간별 날씨 정보를 가져오는 함수입니다.
+  const fetchHourlyWeatherInfo = async (lat, lng) => {
+    const API_KEY = "e7f59aaca8543637eab9ad2b801f9249";
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}&lang=kr`
+    );
+    const data = await response.json();
+    return data;
+  };
+
+  // 정보 창을 토글하는 함수입니다.
+  const toggleInfoWindow = (map, marker, infowindow) => {
+    if (infowindow.getMap()) {
+      infowindow.close();
+    } else {
+      infowindow.open(map, marker);
+    }
+  };
+
+  // 마커 위에 정보를 표시하는 함수입니다.
+  const showInfoOnMarker = async (lat, lng, resultAddress, marker, map) => {
+    const weatherInfo = await fetchWeatherInfo(lat, lng);
+    const temperature = weatherInfo.main.temp;
+    const description = weatherInfo.weather[0].description;
+    const humidity = weatherInfo.main.humidity;
+    const content = `<div style="white-space: nowrap;">지번 주소: ${resultAddress}<br>온도: ${temperature}°C<br>날씨: ${description}<br>습도: ${humidity}%</div>`;
+    const infowindow = new window.kakao.maps.InfoWindow({
+      content,
+    });
+    infowindowsRef.current.push(infowindow);
+    infowindow.open(map, marker);
+  };
+
   useEffect(() => {
-    // Kakao Map SDK 스크립트 로드
+    // 카카오 맵 API 스크립트를 로드합니다.
     const script = document.createElement("script");
     script.async = true;
     script.src =
@@ -37,35 +81,41 @@ const WeatherInformation = () => {
         mapRef.current = map;
         placesRef.current = new window.kakao.maps.services.Places();
 
-        // 지도 클릭 이벤트 리스너
+        // 지도를 클릭했을 때의 이벤트 핸들러입니다.
         window.kakao.maps.event.addListener(
           map,
           "click",
           async function (mouseEvent) {
-            // 기존 마커 제거
             markersRef.current.forEach((marker) => marker.setMap(null));
+            infowindowsRef.current.forEach((info) => info.close());
             markersRef.current = [];
+            infowindowsRef.current = [];
 
             const latlng = mouseEvent.latLng;
             const marker = new window.kakao.maps.Marker({ position: latlng });
             marker.setMap(map);
             markersRef.current.push(marker);
 
-            // 클릭한 위치의 주소 정보 및 날씨 정보 가져오기
             const geocoder = new window.kakao.maps.services.Geocoder();
             geocoder.coord2Address(
               latlng.getLng(),
               latlng.getLat(),
               async function (result, status) {
                 if (status === window.kakao.maps.services.Status.OK) {
-                  const weatherInfo = await fetchWeatherInfo(
+                  showInfoOnMarker(
+                    latlng.getLat(),
+                    latlng.getLng(),
+                    result[0].address.address_name,
+                    marker,
+                    map
+                  );
+
+                  // 시간별 날씨 정보를 가져옵니다.
+                  const hourlyWeatherInfo = await fetchHourlyWeatherInfo(
                     latlng.getLat(),
                     latlng.getLng()
                   );
-                  const temperature = weatherInfo.main.temp;
-                  const description = weatherInfo.weather[0].description;
-                  const content = `<div>지번 주소: ${result[0].address.address_name}<br>온도: ${temperature}°C<br>날씨: ${description}</div>`;
-                  setPlaceInfo(content);
+                  setHourlyForecast(hourlyWeatherInfo.list.slice(0, 48));
                 }
               }
             );
@@ -75,15 +125,16 @@ const WeatherInformation = () => {
     };
   }, []);
 
-  // 검색 기능: 입력된 키워드로 위치를 검색하고 해당 위치에 마커를 표시하는 함수
+  // 검색어를 입력하여 해당 지역의 날씨 정보를 가져옵니다.
   const searchPlaces = () => {
     const keyword = searchRef.current.value;
 
     placesRef.current.keywordSearch(keyword, async (results, status) => {
       if (status === window.kakao.maps.services.Status.OK) {
-        // 기존 마커 제거
         markersRef.current.forEach((marker) => marker.setMap(null));
+        infowindowsRef.current.forEach((info) => info.close());
         markersRef.current = [];
+        infowindowsRef.current = [];
 
         const bounds = new window.kakao.maps.LatLngBounds();
 
@@ -97,33 +148,80 @@ const WeatherInformation = () => {
           marker.setMap(mapRef.current);
           markersRef.current.push(marker);
 
-          // 날씨 정보 가져오기
-          const weatherInfo = await fetchWeatherInfo(result.y, result.x);
-          const temperature = weatherInfo.main.temp;
-          const description = weatherInfo.weather[0].description;
-          const content = `<div>지번 주소: ${result.address_name}<br>온도: ${temperature}°C<br>날씨: ${description}</div>`;
-          setPlaceInfo(content);
-        }
+          // 마커를 클릭했을 때의 이벤트 핸들러입니다.
+          window.kakao.maps.event.addListener(
+            marker,
+            "click",
+            async function () {
+              // 이전 정보창들을 모두 닫습니다.
+              infowindowsRef.current.forEach((info) => info.close());
+              showInfoOnMarker(
+                result.y,
+                result.x,
+                result.address_name,
+                marker,
+                mapRef.current
+              );
 
-        mapRef.current.setBounds(bounds);
+              const hourlyWeatherInfoForSearch = await fetchHourlyWeatherInfo(
+                result.y,
+                result.x
+              );
+              setHourlyForecast(hourlyWeatherInfoForSearch.list.slice(0, 48));
+            }
+          );
+
+          mapRef.current.setBounds(bounds);
+        }
       } else {
         alert("검색 결과가 존재하지 않습니다.");
       }
     });
   };
 
+  const handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      searchPlaces();
+    }
+  };
+
   return (
     <div className="weatherinfo">
       <h4>날씨 정보</h4>
-      <input type="text" placeholder="지역 검색..." ref={searchRef} />
-      <button onClick={searchPlaces}>검색</button>
+      <div className="info-container">
+        <div className="hourly-forecast-slider">
+          <button onClick={() => slideForecast("left")}>&lt;</button>
+          <div className="hourly-forecast">
+            {hourlyForecast
+              .slice(forecastStart, forecastStart + 5)
+              .map((forecast, index) => (
+                <div key={index} className="forecast-item">
+                  <div>
+                    {`${
+                      new Date(forecast.dt * 1000).getMonth() + 1
+                    }월 ${new Date(forecast.dt * 1000).getDate()}일 ${new Date(
+                      forecast.dt * 1000
+                    ).getHours()}시`}
+                  </div>
+                  <div>{forecast.main.temp}°C</div>
+                  <div>{forecast.weather[0].description}</div>
+                  <div>{forecast.main.humidity}%</div>
+                </div>
+              ))}
+          </div>
+          <button onClick={() => slideForecast("right")}>&gt;</button>
+        </div>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="지역 검색..."
+            ref={searchRef}
+            onKeyUp={handleKeyUp}
+          />
+          <button onClick={searchPlaces}>검색</button>
+        </div>
+      </div>
       <div className="map" id="kakao-map"></div>
-      {placeInfo && (
-        <div
-          className="place-info"
-          dangerouslySetInnerHTML={{ __html: placeInfo }}
-        />
-      )}
     </div>
   );
 };
